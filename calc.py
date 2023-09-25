b5894e74d0a30ccc1972cc5a12cb164e47682c8b
DanielMashmaniuk <82703760+DanielMashmaniuk@users.noreply.github.com>
2023-09-25 21:32:02 +0300

    1

---
+++ calc.py
@@ -0,0 +1,76 @@
+from kivy.app import App 
+
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+
+from kivy.config import Config
+Config.set("graphics","resizable",0)
+Config.set("graphics","weidth",400)
+Config.set("graphics","heigh",500)
+
+class CalculateApp(App):
+    def update_label(self):
+        self.lbl.text = self.formula
+        
+    def add_number(self, instance):
+        if(self.formula == "0"):
+            self.formula = ""  
+            
+        self.formula += str(instance.text)
+        self.update_label()  
+        
+    def add_operation(self, instance):
+        if(str(instance.text).lower() == "x"):
+            self.formula +="*"
+        else:
+            self.formula += str(instance.text)
+            
+        self.update_label()
+    
+    def calc_result(self, instance):
+        if self.null in self.lbl.text:
+            self.lbl.text = "На нуль ділити не можна"
+            self.formula = "0"
+        else:
+            self.lbl.text = str(eval(self.lbl.text))
+            self.formula = "0"
+        
+    def build(self):
+        self.null = "/0"
+        self.formula ="0"
+        
+        bl = BoxLayout(orientation = "vertical", padding = 25)
+        gl = GridLayout(cols = 4, spacing = 3, size_hint = (1 , .6))
+        
+        self.lbl = Label(text="0", font_size = 40, halign="right", valign="center", size_hint =(1, .4))
+        bl.add_widget(self.lbl)
+        
+        gl.add_widget( Button(text = "7", on_press = self.add_number))
+        gl.add_widget( Button(text = "9", on_press = self.add_number))
+        gl.add_widget( Button(text = "8", on_press = self.add_number))
+        gl.add_widget( Button(text = "X", on_press = self.add_operation))
+        
+        gl.add_widget( Button(text = "4", on_press = self.add_number))
+        gl.add_widget( Button(text = "5", on_press = self.add_number))
+        gl.add_widget( Button(text = "6", on_press = self.add_number))
+        gl.add_widget( Button(text = "-", on_press = self.add_operation))
+        
+        gl.add_widget( Button(text = "1", on_press = self.add_number))
+        gl.add_widget( Button(text = "2", on_press = self.add_number))
+        gl.add_widget( Button(text = "3", on_press = self.add_number))
+        gl.add_widget( Button(text = "+", on_press = self.add_operation))
+        
+        gl.add_widget( Button(text = "/", on_press = self.add_operation))
+        gl.add_widget( Button(text = "0", on_press = self.add_number))
+        gl.add_widget( Button(text = ".", on_press = self.add_number))
+        gl.add_widget( Button(text = "=", on_press = self.calc_result))
+        
+        bl.add_widget(gl)
+        return bl
+    
+if __name__ == "__main__":
+    CalculateApp().run()
